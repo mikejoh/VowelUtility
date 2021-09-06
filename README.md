f05ad7640ce11a242dcb71b33c149a8762445f3d
Mikael Johansson <mik.json@gmail.com>
2021-09-06 22:17:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# VowelUtilty
+
+`VowelUtility` is a library used for counting vowels and various other vowel related operations.
+
+_This repository was created to explore how the .NET C# development ecosystem looks like._
---
+++ VowelUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VowelUtility
+{
+    public static class Vowel
+    {
+        public static int Count(string sentence)
+        {
+            int total = 0;
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i]  == 'a' || sentence[i] == 'e' || sentence[i] == 'i' || sentence[i] == 'o' || sentence[i] == 'u')
+                {
+                    total++;
+                }
+            }
+
+            return total;
+        }
+    }
+}
